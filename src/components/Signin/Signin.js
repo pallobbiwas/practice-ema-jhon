a00827c0a2d6223678b3d858a9e685f1bf879bea
pallobbiswas <pallobbiswas326@gmail.com>
2022-04-10 12:45:37 +0600

    login route done

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,19 +1,51 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { Link, useNavigate } from "react-router-dom";
+import auth from "../../firebase.init";
 import "./Signin.css";
 
 const Signin = () => {
+  const [signInWithEmailAndPassword, user] = useSignInWithEmailAndPassword(auth);
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const navigate = useNavigate()
+  //blur input
+  const emailBlur = (e) => {
+    setEmail(e.target.value);
+  };
+  const passBluer = (e) => {
+    setPass(e.target.value);
+  };
+  const fromSubmit = (e) =>{
+    e.preventDefault();
+    signInWithEmailAndPassword(email, pass)
+  }
+  if(user){
+    navigate('/shop')
+  }
   return (
     <div className="form-container">
       <div>
         <h2 className="center">SignIn</h2>
         <hr />
-        <form action="">
+        <form onSubmit={fromSubmit} action="">
           <label htmlFor="email">email</label>
-          <input type="email" name="emaile" id="" placeholder="email" />
+          <input
+            onBlur={emailBlur}
+            type="email"
+            name="emaile"
+            id=""
+            placeholder="email"
+          />
 
           <label htmlFor="password">password</label>
-          <input type="password" name="password" id="" placeholder="password" />
+          <input
+            onBlur={passBluer}
+            type="password"
+            name="password"
+            id=""
+            placeholder="password"
+          />
 
           <div className="btn">
             <button>Signin</button>
@@ -27,7 +59,7 @@ const Signin = () => {
         </p>
       </div>
       <div className="google">
-          <button>signin with google</button>
+        <button>signin with google</button>
       </div>
     </div>
   );
