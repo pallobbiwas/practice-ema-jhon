10b922078f9f541abfb0ee3be90d48fedf6e4112
pallobbiswas <pallobbiswas326@gmail.com>
2022-04-10 12:09:12 +0600

    added

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,23 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import auth from "../../firebase.init";
 
 const Signup = () => {
+  const [createUserWithEmailAndPassword, user] =
+    useCreateUserWithEmailAndPassword(auth);
+    const navigate = useNavigate()
+  // state for input value
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [rePass, setRePass] = useState("");
+  const [errors, seterrors] = useState("");
+
+  //onblur function
+  const emailBlur = (e) => {
+    setEmail(e.target.value);
+  };
+  const nameBlur = (e) => {
+    setName(e.target.value);
+  };
+  const PasswordBlur = (e) => {
+    setPassword(e.target.value);
+  };
+  const rePasswordBlur = (e) => {
+    setRePass(e.target.value);
+  };
+  const formSubmit = (e) => {
+    e.preventDefault();
+    if (password !== rePass) {
+      seterrors("password not match");
+      return;
+    }
+    if (password < 6) {
+      seterrors("password contain should be 6 charrector");
+      return;
+    }
+    createUserWithEmailAndPassword(email, password, name);
+  };
+  if(user){
+    navigate('/signin')
+  }
+
   return (
     <div>
       <div className="form-container">
         <h1>sign up</h1>
         <hr />
-        <form action="">
+        <form onSubmit={formSubmit} action="">
           <label htmlFor="email">email</label>
-          <input type="email" name="emaile" id="" placeholder="email" />
+          <input
+            onBlur={emailBlur}
+            type="email"
+            name="emaile"
+            id=""
+            placeholder="email"
+            required
+          />
 
           <label htmlFor="name">name</label>
-          <input type="text" name="name" id="" placeholder="name" />
+          <input
+            onBlur={nameBlur}
+            type="text"
+            name="name"
+            id=""
+            placeholder="name"
+            required
+          />
 
           <label htmlFor="password">password</label>
-          <input type="password" name="password" id="" placeholder="password" />
+          <input
+            onBlur={PasswordBlur}
+            type="password"
+            name="password"
+            id=""
+            placeholder="password"
+            required
+          />
 
+          <p>{errors}</p>
           <label htmlFor="re-password">re-password</label>
-          <input type="password" name="re-password" id="" placeholder="re-password" />
+          <input
+            onBlur={rePasswordBlur}
+            type="password"
+            name="re-password"
+            id=""
+            placeholder="re-password"
+            required
+          />
 
           <div className="btn">
             <button>Signup</button>
